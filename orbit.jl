662cb4f44f1ead075b16978ea204b6762e66da56
Isaia Nisoli <nisoli@im.ufrj.br>
2024-06-25 03:13:04 -0300

    Added Lecture Notes 1

--- orbit.jl
+++ orbit.jl
@@ -1,14 +1,4 @@
-function orbit(T, x0::Float64, N)
-    orb = zeros(Float64, N)
-    x = x0
-    for i in 1:N
-        orb[i] = x
-        x = T(x)
-    end
-    return orb
-end
-
-function orbit(T, x0, N)
+function orbit3(T, x0, N)
     orb = zeros(typeof(x0), N)
     x = x0
     for i in 1:N
